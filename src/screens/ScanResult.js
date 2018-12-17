3758260d8b2eb63c315b0868db34e70dc8e2b1c7
zgatrying <279501822@qq.com>
2018-12-17 13:28:56 +0800

    新增扫码结果页组件卸载时使能扫码功能

--- src/screens/ScanResult.js
+++ src/screens/ScanResult.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import NavigationService from '../NavigationService';
 export default class ScanResult extends Component {
+
+  componentWillUnmount() {
+    let params = this.props.navigation.state.params;
+    if(params) {
+      params.enableBarcodeRead && params.enableBarcodeRead()
+    }
+  }
+
   render() {
     const scanResult = NavigationService.getParam('info');
     return (
